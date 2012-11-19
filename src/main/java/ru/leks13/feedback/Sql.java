29e8153a4f9fe1266621cfe8c5934847938e831d
Leks13 <admin@leks13.ru>
2012-11-19 23:12:31 +0400

    Security fix

--- src/main/java/ru/leks13/feedback/Sql.java
+++ src/main/java/ru/leks13/feedback/Sql.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
@@ -51,6 +52,7 @@ public class Sql {
             java.sql.Statement st = (java.sql.Statement) connection.createStatement();
             query = "create table feedback (id serial,mail varchar(50), phone varchar(12), message varchar(1000), status varchar(15));";
             st.execute(query);
+            st.close();
         } catch (SQLException e) {
             if (e.toString().equals("org.postgresql.util.PSQLException: ERROR: relation \"feedback\" already exists")) {
                 System.out.println("INFO: Base exists");
@@ -61,52 +63,70 @@ public class Sql {
     }
 
     public void addMessage(String mail, String phone, String message) throws ClassNotFoundException, SQLException {
+
         Class.forName("org.postgresql.Driver");
-        String query;
-        Connection connection;
-        connection = DriverManager.getConnection("jdbc:postgresql://" + ip + ":" + port + "/" + base, user, password);
-        boolean res;
-        java.sql.Statement st = (java.sql.Statement) connection.createStatement();
-        query = "insert into feedback (mail,phone,message,status) values('" + mail + "','" + phone + "','" + message + "','Not processed');";
-        st.execute(query);
-        st.close();
-        connection.close();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://" + ip + ":" + port + "/" + base, user, password);
+            boolean res;
+            java.sql.Statement st = (java.sql.Statement) connection.createStatement();
+            PreparedStatement ps = connection.prepareStatement("insert into feedback (mail,phone,message,status) values(?,?,?,?);");
+            ps.setString(1, mail);
+            ps.setString(2, phone);
+            ps.setString(3, message);
+            ps.setString(4, "Not processed");
+            ps.execute();
+            st.close();
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Sql.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            connection.close();
+        }
     }
 
     public String listOfMessage() throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
         ResultSet rs;
-        String query;
-        Connection connection;
-        connection = DriverManager.getConnection("jdbc:postgresql://" + ip + ":" + port + "/" + base, user, password);
         String res = "";
-        java.sql.Statement st = (java.sql.Statement) connection.createStatement();
-        query = "select * from \"public\".feedback";
-        rs = st.executeQuery(query);
-        while (rs.next()) {
-            String id = rs.getString("id");
-            String mail = rs.getString("mail");
-            String phone = rs.getString("phone");
-            String message = rs.getString("message");
-            String status = rs.getString("status");
-            res += "<tr><td>" + id + "</td><td>" + mail + "</td><td>" + phone + "</td><td>" + message + "</td><td>" + status + "</td></tr><br>";
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://" + ip + ":" + port + "/" + base, user, password);
+            java.sql.Statement st = (java.sql.Statement) connection.createStatement();
+            String query = "select * from \"public\".feedback";
+            rs = st.executeQuery(query);
+            while (rs.next()) {
+                String id = rs.getString("id");
+                String mail = rs.getString("mail");
+                String phone = rs.getString("phone");
+                String message = rs.getString("message");
+                String status = rs.getString("status");
+                res += "<tr><td>" + id + "</td><td>" + mail + "</td><td>" + phone + "</td><td>" + message + "</td><td>" + status + "</td></tr><br>";
+            }
+            st.close();
+        } finally {
+            connection.close();
         }
-        st.close();
-        connection.close();
         return res;
     }
 
     void changeStatus(String id, String status) throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
         String query;
-        Connection connection;
-        connection = DriverManager.getConnection("jdbc:postgresql://" + ip + ":" + port + "/" + base, user, password);
-        boolean res;
-        java.sql.Statement st = (java.sql.Statement) connection.createStatement();
-        query = "UPDATE feedback SET status = '" + status + "' WHERE id = " + id + ";";
-        st.execute(query);
-        st.close();
-        connection.close();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://" + ip + ":" + port + "/" + base, user, password);
+            boolean res;
+            int i = Integer.parseInt(id);
+            java.sql.Statement st = (java.sql.Statement) connection.createStatement();
+            PreparedStatement ps = connection.prepareStatement("UPDATE feedback SET status =? WHERE id =?;");
+            ps.setString(1, status);
+            ps.setInt(2, i);
+            ps.execute();
+            st.close();
+        } finally {
+            connection.close();
+        }
 
     }
 }
